7d95596e5052564f643928bcdc42efe584479bb3
Unknown <marianoegam@gmail.com>
2017-05-25 18:13:11 -0300

    Secuencias máximas

--- OIA/src/oia/Test.java
+++ OIA/src/oia/Test.java
@@ -4,10 +4,14 @@ import java.io.File;
 
 public class Test {
 	public static void main(String[] args) {
-		File entrada = new File("c:\\Users\\mariano.e.gambacorta\\Documents\\Estudio\\Programacion\\IN\\PALIN.in");
-		File salida = new File("c:\\Users\\mariano.e.gambacorta\\Documents\\Estudio\\Programacion\\OUT\\PALIN.out");
+//		String archivo = "PALIN";
+		String archivo = "SECMAX";
 		
-		EjercicioOIA ejercicio = new Palindromos(entrada, salida);
+		File entrada = new File("c:\\Users\\mariano.e.gambacorta\\Documents\\Estudio\\Programacion\\IN\\" + archivo + ".in");
+		File salida = new File("c:\\Users\\mariano.e.gambacorta\\Documents\\Estudio\\Programacion\\OUT\\" + archivo + ".out");
+		
+//		EjercicioOIA ejercicio = new Palindromos(entrada, salida);
+		EjercicioOIA ejercicio = new SecuenciasMaximas(entrada, salida);
 		
 		ejercicio.resolver();
 	}

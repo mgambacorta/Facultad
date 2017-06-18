4b7761e2b9205528febac4d89cb38adc2d31425a
Unknown <marianoegam@gmail.com>
2017-06-18 11:21:54 -0300

    Mas generadores de grafos

--- Grafos/src/grafos/GrafoNPartito.java
+++ Grafos/src/grafos/GrafoNPartito.java
@@ -11,8 +11,27 @@ public class GrafoNPartito extends Grafo {
 
 	@Override
 	public void generar() {
-		// TODO Auto-generated method stub
+		int grupo = 1;
+		int vecGrupos[] = new int[this.orden + 1]; //el cero lo voy a ignorar
 		
+		//armo los grupos
+		for(int i = 1; i <= this.orden; i++) {
+			vecGrupos[i] = grupo;
+			
+			if(grupo < this.particion) {
+				grupo++;
+			} else {
+				grupo = 1;
+			}
+		}
+		
+		// Si los nodos partenecen a grupos distintos los uno
+		for(int i = 1; i < this.orden; i++) {
+			for( int j = i; j <= this.orden; j++) {
+				if( vecGrupos[i] != vecGrupos[j] ) {
+					this.grafo.setValorArista(i, j, 1);
+				}
+			}
+		}
 	}
-	
 }
